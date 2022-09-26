d88c8be1cee1fe5115ec241276dcb2978f696817
Yashraj Metkar <metkaryashraj@gmail.com>
2022-09-26 23:37:18 +0530

    Turtle Crossing Game

---
+++ Turtle Crossing Game/player.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+STARTINIG_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTINIG_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
